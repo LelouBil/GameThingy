0f8047ad8bb130cf7a0792a8f220b99ca47d32f1
Ladislawa <nirina.bellegarde@gmail.com>
2020-02-20 18:42:34 +0100

    renomage

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculduscore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    { 
+
+    }
+    string excellent = "excellent";
+    string good = "good";
+    string meh = "meh";
+
+    int score;
+
+    // Update is called once per frame
+    void Updatescore(float time)
+    {
+
+        if (time <= 0.2) {
+            Popup(excellent);
+            score=score+3;
+        }
+        if (time <= 0.4 && time > 0.2)
+        {
+            Popup(good);
+            score = score + 2;
+        }
+        if (time <= 0.5 && time > 0.4)
+        {
+            Popup("meh");
+            score++;
+        }
+    }
+
+    void Popup(string msg)
+    {
+        Debug.log();
+    }
+
+}
