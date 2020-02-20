f7279cd3d9c176773f00ca1ce9a50e803e0cea08
LelouBil <bilel.medimegh@gmail.com>
2020-02-20 15:08:10 +0100

    menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+
+    public void Quiter()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Scenes/Game");
+    }
+   
+}
